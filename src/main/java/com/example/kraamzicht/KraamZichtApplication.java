6f50122a00902fcab12c31e843ebacbf02a9581a
108nandita <108nandita@gmail.com>
2023-11-04 11:20:40 +0100

    initializing project

---
+++ src/main/java/com/example/kraamzicht/KraamZichtApplication.java
@@ -0,0 +1,13 @@
+package com.example.kraamzicht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KraamZichtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KraamZichtApplication.class, args);
+    }
+
+}
